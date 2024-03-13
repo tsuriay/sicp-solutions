02318f6237a31a61ad26eae38e283db4445389b4
Олег О. Кустов <ookustov@yoomoney.ru>
2024-03-13 18:57:26 +0300

    Fix solution 02-23

--- 02/02-23.scm
+++ 02/02-23.scm
@@ -1,7 +1,6 @@
 (define (for-each proc items)
-  (if (null? items)
-	#t
-	(proc (car items)) (for-each proc (cdr items))))
+  (cond ((null? items) #t)
+		(else (proc (car items)) (for-each proc (cdr items)))))
 
 (for-each (lambda (x) (newline) (display x))
 		  (list 57 321 88))
